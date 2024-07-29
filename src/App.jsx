469b3df59fc51c0121076773fc6a0ab7619c6584
Youssef Saad <youssef.saad33218@gmail.com>
2024-07-29 21:02:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "./components/container";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Tic-Tac-Toe</h1>
+      </header>
+      <Container/>
+    </>
+  );
+}
+
+export default App;
